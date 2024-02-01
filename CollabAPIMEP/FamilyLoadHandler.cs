7006898154f9bd7aeb8a74e9201697fa49ba8787
Api Objects <129112417+ApiObjects@users.noreply.github.com>
2024-02-01 15:14:18 +0100

    apply rules method finished and tested

--- CollabAPIMEP/FamilyLoadHandler.cs
+++ CollabAPIMEP/FamilyLoadHandler.cs
@@ -5,10 +5,13 @@ using Autodesk.Revit.UI;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using System.Security.Policy;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace CollabAPIMEP
 {
@@ -55,12 +58,14 @@ namespace CollabAPIMEP
             }
         }
 
-        public void ApplyRules(string pathname)
+        public void ApplyRules(string pathname, FamilyLoadingIntoDocumentEventArgs e)
         {
             if (RulesMap == null)
             {
                 return;
             }
+
+
             //Schema schema = Schema.Lookup(Settings);
             //if (schema == null)
             //{
@@ -71,6 +76,9 @@ namespace CollabAPIMEP
             string errorMessage = "";
 
             Document familyDocument = m_app.OpenDocumentFile(pathname);
+            FamilyManager familyManager = familyDocument.FamilyManager;
+
+
             foreach (Rule rule in rules)
             {
                 if (!rule.IsEnabled)
@@ -80,15 +88,28 @@ namespace CollabAPIMEP
 
                 switch (rule.ID)
                 {
+
+
                     case "NumberOfElements":
-                        FilteredElementCollector eleCol = new FilteredElementCollector(familyDocument);
-                        var elements = eleCol.WhereElementIsNotElementType().ToElements();
-                        int elementCount = elements.Count;
-                        if (elementCount > Convert.ToInt32(rule.UserInput))
+
+                        var maxElements = Convert.ToInt32(rule.UserInput);
+
+
+
+                        FilteredElementCollector collectorElements = new FilteredElementCollector(familyDocument);
+
+                        // get nested families and modeled geometry
+                        FilterRule parRuleVisibility = ParameterFilterRuleFactory.CreateHasValueParameterRule(new ElementId(((int)BuiltInParameter.IS_VISIBLE_PARAM)));
+
+                        ElementParameterFilter filterVisibility = new ElementParameterFilter(parRuleVisibility);
+
+                        IList<Element> elementsWithGeometry = collectorElements.WherePasses(filterVisibility).ToElements();
+
+                        int elementCount = elementsWithGeometry.Count;
+                        if (elementCount > maxElements)
                         {
                             ruleViolation = true;
-                            familyDocument.Close(false);
-                            errorMessage += $"{elementCount} elements inside family, loading family canceled" + System.Environment.NewLine;
+                            errorMessage += $"- too many elements inside family ({elementCount}, only {maxElements} allowed" + System.Environment.NewLine;
                         }
                         break;
                     case "ImportedInstances":
@@ -97,23 +118,64 @@ namespace CollabAPIMEP
                         int importCount = importsLinks.Count;
                         if (importCount > 0)
                         {
-                            familyDocument.Close(false);
-                            errorMessage += $"{importCount} imported instances inside family, loading family canceled" + System.Environment.NewLine;
+                            ruleViolation = true;
+
+                            errorMessage += $"- too many imported instances inside family ({importCount})" + System.Environment.NewLine;
                         }
                         break;
                     case "SubCategory":
-                        //FilteredElementCollector colImportsAll = new FilteredElementCollector(familyDocument).OfClass(typeof(ImportInstance));
-                        //IList<Element> importsLinks = colImportsAll.WhereElementIsNotElementType().ToElements();
+
+                        // Create a FilteredElementCollector to collect elements from the document
+                        FilteredElementCollector collector = new FilteredElementCollector(familyDocument);
+
+                        // Create a quick filter rule
+                        FilterRule parRule = ParameterFilterRuleFactory.CreateHasValueParameterRule(new ElementId(((int)BuiltInParameter.FAMILY_ELEM_SUBCATEGORY)));
+
+      
+                        // Create a rule to check if the parameter has any value (not null or empty)
+
+                        // Create a filter element with the rule
+                        ElementParameterFilter filter = new ElementParameterFilter(parRule);
+
+                        IList<Element> filteredElements = collector.WherePasses(filter).ToElements();
+                        int elementsWithCat = 0;
+
+                        foreach(Element element in filteredElements)
+                        {
+                            ElementId eleId = element.get_Parameter(BuiltInParameter.FAMILY_ELEM_SUBCATEGORY).AsElementId();
+                            if(eleId == ElementId.InvalidElementId)
+                            {
+                                ruleViolation = true;
+                                errorMessage += "- elements without subcategory found" + System.Environment.NewLine;
+                            }
+                        }
+
                         break;
                     case "Material":
+
+                        var maxMaterials = Convert.ToInt32(rule.UserInput);
+
+                        FilteredElementCollector materialCollector = new FilteredElementCollector(familyDocument).OfClass(typeof(Material));
+                        IList<Element> materials = materialCollector.ToElements();
+
+                        if (materials.Count > Convert.ToInt32(rule.UserInput))
+                        {
+                            ruleViolation = true;
+                            errorMessage += $"- too many materials inside family ({materials.Count}, only {maxMaterials} allowed)" + System.Environment.NewLine;
+                        }
+
+
                         break;
                 }
 
-                if (ruleViolation == true)
-                {
-                    throw new RuleException(errorMessage);
+            }
 
-                }
+            if (ruleViolation == true)
+            {
+                //familyDocument.Close(false);
+                errorMessage = $"family: '{e.FamilyName}' load canceled because:" + System.Environment.NewLine + errorMessage;
+                throw new RuleException(errorMessage);
+
             }
         }
 
@@ -212,15 +274,18 @@ namespace CollabAPIMEP
             }
             string pathname = e.FamilyPath + e.FamilyName + ".rfa";
 
+            
+
             try
             {
-                ApplyRules(pathname);
+                ApplyRules(pathname, e);
             }
             catch (RuleException ex)
             {
                 e.Cancel();
                 Results.Add("Canceled: " + e.FamilyPath + e.FamilyName + ".rfa");
                 MessageBox.Show(ex.Message);
+                e.Cancel();
             }
 
             //Document familyDocument = m_app.OpenDocumentFile(pathname);
