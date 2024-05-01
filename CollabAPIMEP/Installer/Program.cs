a9e6c054e153507bd8b79ae447235a076edaaff0
Api Objects <129112417+ApiObjects@users.noreply.github.com>
2024-05-01 23:04:12 +0200

    mappenstructuur

--- CollabAPIMEP/Installer/Program.cs
+++ CollabAPIMEP/Installer/Program.cs
@@ -1,54 +1,98 @@
-﻿using Installer.Dialogs;
-using System;
+﻿using System;
+using System.Windows.Forms;
 using WixSharp;
+using WixSharp.Forms;
 
-namespace Installer
+namespace WixSharp_Installer
 {
     internal class Program
     {
+        const string publisher = "MEPAPI";
+        const string appGuid = "537eb34b-01f8-4262-9b24-f533aee21a53";
         static void Main()
         {
-            var project = new ManagedProject("MyProduct",
-                             new Dir(@"%ProgramFiles%\My Company\My Product",
-                                 new File("Program.cs")));
+            string filedir = @"C:\Users\taco\source\repos\MepoverRevit\bin\Release\";
 
-            project.GUID = new Guid("6fe30b47-2577-43ad-9095-1861ba25889b");
+            var project = new ManagedProject("CollabAPIMEP",
+                             new Dir(@"C:\ProgramData\Autodesk\Revit\Addins",
+                                new Dir(@"2022",
+                                    new File(@"C:\Users\arjan\source\repos\MEPAPI\Project Guard\SharedProject\resources\ProjectMonitor_2022.addin"),
+                                    new Dir(@"MEPAPI",
+                                        new File(filedir + "MepoverRevit.2022.dll"))),
+                                new Dir(@"2023",
+                                    new File(@"C:\Users\arjan\source\repos\MEPAPI\Project Guard\SharedProject\resources\ProjectMonitor_2023.addin"),
+                                    new Dir(@"MEPAPI",
+                                        new File(filedir + "MepoverRevit.2023.dll"))),
+                                new Dir(@"2024",
+                                    new File(@"C:\Users\arjan\source\repos\MEPAPI\Project Guard\SharedProject\resources\ProjectMonitor_2024.addin"),
+                                    new Dir(@"MEPAPI",
+                                        new File(filedir + "MepoverRevit.2024.dll")))))
+            {
+                Scope = InstallScope.perUser,
+                GUID = new Guid(appGuid),
+                Version = new Version("0.1.0")
+            };
+
+            //project.Package.AttributesDefinition = "InstallPrivileges=limited";
+            project.ControlPanelInfo.Manufacturer = publisher;
+            project.ControlPanelInfo.Contact = "Taco Pover / Arjan Noya";
+
+            //project.ManagedUI = ManagedUI.Empty;    //no standard UI dialogs
+            //project.ManagedUI = ManagedUI.Default;  //all standard UI dialogs
 
             //custom set of standard UI dialogs
             project.ManagedUI = new ManagedUI();
 
-            project.ManagedUI.InstallDialogs.Add<WelcomeDialog>()
-                                            .Add<LicenceDialog>()
-                                            .Add<SetupTypeDialog>()
-                                            .Add<FeaturesDialog>()
-                                            .Add<InstallDirDialog>()
-                                            .Add<ProgressDialog>()
-                                            .Add<ExitDialog>();
+            project.ManagedUI.InstallDialogs.Add(Dialogs.Welcome)
+                                            .Add(Dialogs.Licence)
+                                            //.Add(Dialogs.SetupType)
+                                            //.Add(Dialogs.Features)
+                                            //.Add(Dialogs.InstallDir)
+                                            .Add(Dialogs.Progress)
+                                            .Add(Dialogs.Exit);
 
-            project.ManagedUI.ModifyDialogs.Add<MaintenanceTypeDialog>()
-                                           .Add<FeaturesDialog>()
-                                           .Add<ProgressDialog>()
-                                           .Add<ExitDialog>();
+            //project.ManagedUI.ModifyDialogs.Add(Dialogs.MaintenanceType)
+            //                               .Add(Dialogs.Features)
+            //                               .Add(Dialogs.Progress)
+            //                               .Add(Dialogs.Exit);
+
+            project.MajorUpgrade = new MajorUpgrade()
+            {
+                Schedule = UpgradeSchedule.afterInstallInitialize,
+                DowngradeErrorMessage = $"A later version of {publisher} is already installed. Setup will now exit."
+            };
+
+            project.Load += Msi_Load;
+            project.BeforeInstall += Msi_BeforeInstall;
+            project.AfterInstall += Msi_AfterInstall;
+            //project.LicenceFile = @"C:\Users\taco\OneDrive - MEPover\Revit\License file.rtf";
+            //project.BannerImage = @"C:\Users\taco\OneDrive - MEPover\Taco\dev\resources\Icons\MEPover banner.png";
+            //project.ValidateBackgroundImage = false;
+            //project.BackgroundImage = @"C:\Users\taco\OneDrive - MEPover\Taco\dev\resources\Icons\MEPover background image.png";
 
             //project.SourceBaseDir = "<input dir path>";
             //project.OutDir = "<output dir path>";
 
-            ValidateAssemblyCompatibility();
-
             project.BuildMsi();
         }
 
-        static void ValidateAssemblyCompatibility()
+        static void Msi_Load(SetupEventArgs e)
         {
-            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
-
-            if (!assembly.ImageRuntimeVersion.StartsWith("v2."))
-            {
-                Console.WriteLine("Warning: assembly '{0}' is compiled for {1} runtime, which may not be compatible with the CLR version hosted by MSI. " +
-                                  "The incompatibility is particularly possible for the EmbeddedUI scenarios. " +
-                                   "The safest way to solve the problem is to compile the assembly for v3.5 Target Framework.",
-                                   assembly.GetName().Name, assembly.ImageRuntimeVersion);
-            }
+            if (!e.IsUISupressed && !e.IsUninstalling)
+                MessageBox.Show(e.ToString(), "Load");
         }
+
+        static void Msi_BeforeInstall(SetupEventArgs e)
+        {
+            if (!e.IsUISupressed && !e.IsUninstalling)
+                MessageBox.Show(e.ToString(), "BeforeInstall");
+        }
+
+        static void Msi_AfterInstall(SetupEventArgs e)
+        {
+            if (!e.IsUISupressed && !e.IsUninstalling)
+                MessageBox.Show(e.ToString(), "AfterExecute");
+        }
+
     }
 }
