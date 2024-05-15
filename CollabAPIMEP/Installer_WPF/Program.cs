7191554d7670eba3710f3baf83309cec62981486
Api Objects <129112417+ApiObjects@users.noreply.github.com>
2024-05-15 15:44:33 +0200

    WPF Installer Admin & User + Revit versions option

--- CollabAPIMEP/Installer_WPF/Program.cs
+++ CollabAPIMEP/Installer_WPF/Program.cs
@@ -4,46 +4,63 @@ using System.Windows.Forms;
 using WixSharp;
 using WixSharp.Forms;
 
-namespace WixSharp_Installer
+namespace Installer_WPF
 {
     internal class Program
     {
         const string publisher = "MEPAPI";
-        const string appGuid = "537eb34b-01f8-4262-9b24-f533aee21a53";
+        const string appGuidAdmin = "537eb34b-01f8-4262-9b24-f533aee21a53";
+        const string appGuidUser = "d6f7ca5f-93be-424f-8c2d-8bf4668921ca";
+
         static void Main()
         {
             System.Reflection.Assembly executingAssembly = System.Reflection.Assembly.GetExecutingAssembly();
             string executingAssemblyDirectory = System.IO.Path.GetDirectoryName(executingAssembly.Location);
-            string projectDir = System.IO.Path.GetFullPath(System.IO.Path.Combine(executingAssemblyDirectory, @"..\..\..\..\"));
+            string projectDir = System.IO.Path.GetFullPath(System.IO.Path.Combine(executingAssemblyDirectory, @"..\..\..\..\..\"));
 
-            string filedir2022 = System.IO.Path.Combine(projectDir, "CollabAPIMEP_2022", "bin", "Release Admin", "CollabAPIMEP_2022.dll");
-            string filedir2023 = System.IO.Path.Combine(projectDir, "CollabAPIMEP_2023", "bin", "Release Admin", "CollabAPIMEP_2023.dll");
-            string filedir2024 = System.IO.Path.Combine(projectDir, "CollabAPIMEP_2024", "bin", "Release Admin", "CollabAPIMEP_2024.dll");
+            string buildNameAdmin = "Admin";
+            string buildNameUser = "User";
 
+
+            CreateInstaller(buildNameAdmin, projectDir, appGuidAdmin);
+            System.Threading.Thread.Sleep(5000);
+
+            CreateInstaller(buildNameUser, projectDir, appGuidUser);   
+        }
+
+        static void CreateInstaller(string buildName, string projectDir, string guid)
+        {
+
+            string filedir2022_admin = System.IO.Path.Combine(projectDir, "CollabAPIMEP_2022", "bin", "Release " + buildName, "CollabAPIMEP_2022.dll");
+            string filedir2023_admin = System.IO.Path.Combine(projectDir, "CollabAPIMEP_2023", "bin", "Release " + buildName, "CollabAPIMEP_2023.dll");
+            string filedir2024_admin = System.IO.Path.Combine(projectDir, "CollabAPIMEP_2024", "bin", "Release " + buildName, "CollabAPIMEP_2024.dll");
 
             string addinFilePath2022 = System.IO.Path.Combine(projectDir, "CollabAPIMEP", "resources", "FamilyAuditor_2022.addin");
             string addinFilePath2023 = System.IO.Path.Combine(projectDir, "CollabAPIMEP", "resources", "FamilyAuditor_2023.addin");
             string addinFilePath2024 = System.IO.Path.Combine(projectDir, "CollabAPIMEP", "resources", "FamilyAuditor_2024.addin");
 
+            var feature2022 = new Feature("Revit 2022");
+            var feature2023 = new Feature("Revit 2023");
+            var feature2024 = new Feature("Revit 2024");
 
             var project = new ManagedProject("CollabAPIMEP",
                              new Dir(@"C:\ProgramData\Autodesk\Revit\Addins",
-                                new Dir(@"2022",
-                                    new File(addinFilePath2022),
-                                    new Dir(@"MEPAPI",
-                                        new File(filedir2022))),
+                                new Dir(feature2022, @"2022",
+                                    new File(feature2022, addinFilePath2022),
+                                    new Dir(feature2022, @"FamilyAuditor",
+                                        new File(feature2022, filedir2022_admin))),
                                 new Dir(@"2023",
-                                    new File(addinFilePath2023),
-                                    new Dir(@"MEPAPI",
-                                        new File(filedir2023))),
+                                    new File(feature2023, addinFilePath2023),
+                                    new Dir(feature2023, @"FamilyAuditor",
+                                        new File(feature2023, filedir2023_admin))),
                                 new Dir(@"2024",
-                                    new File(addinFilePath2024),
-                                    new Dir(@"MEPAPI",
-                                        new File(filedir2024)))))
+                                    new File(feature2024, addinFilePath2024),
+                                    new Dir(feature2024, @"FamilyAuditor",
+                                        new File(feature2024, filedir2024_admin)))))
             {
                 Scope = InstallScope.perUser,
-                GUID = new Guid(appGuid),
-                Version = new Version("0.1.0")
+                GUID = new Guid(guid),
+                Version = new Version("0.0.1")
             };
 
 
@@ -57,18 +74,18 @@ namespace WixSharp_Installer
             //custom set of standard UI dialogs
             project.ManagedUI = new ManagedUI();
 
-            project.ManagedUI.InstallDialogs.Add(Dialogs.Welcome)
-                                            .Add(Dialogs.Licence)
-                                            //.Add(Dialogs.SetupType)
-                                            //.Add(Dialogs.Features)
-                                            //.Add(Dialogs.InstallDir)
-                                            .Add(Dialogs.Progress)
-                                            .Add(Dialogs.Exit);
+            project.ManagedUI.InstallDialogs.Add<Installer_WPF.WelcomeDialog>()
+                                .Add<Installer_WPF.LicenceDialog>()
+                                .Add<Installer_WPF.FeaturesDialog>()
+                                .Add<Installer_WPF.ProgressDialog>()
+                                .Add<Installer_WPF.ExitDialog>();
 
-            project.ManagedUI.ModifyDialogs.Add(Dialogs.MaintenanceType)
-                                           .Add(Dialogs.Features)
-                                           .Add(Dialogs.Progress)
-                                           .Add(Dialogs.Exit);
+            project.ManagedUI.ModifyDialogs.Add<Installer_WPF.MaintenanceTypeDialog>()
+                                           .Add<Installer_WPF.FeaturesDialog>()
+                                           .Add<Installer_WPF.ProgressDialog>()
+                                           .Add<Installer_WPF.ExitDialog>();
+
+
 
             project.MajorUpgrade = new MajorUpgrade()
             {
@@ -88,7 +105,7 @@ namespace WixSharp_Installer
             //project.SourceBaseDir = "<input dir path>";
             //project.OutDir = "<output dir path>";
 
-            project.BuildMsi();
+            project.BuildMsi("FamilyAuditor_" + buildName + ".msi");
         }
 
         static void Msi_Load(SetupEventArgs e)
